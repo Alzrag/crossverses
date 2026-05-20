e67879ddea3939d99c0e237282ac885e19d28535
Alzrag <jediclark14@icloud.com>
2026-05-20 15:26:42 -0600

    added sliding and tweaked some values movement controller is done except for a issue with wall running and the side way force to wall run causing to much friction to slide down ever

--- Assets/movement shooter/Scripts/MovementControllerMS.cs
+++ Assets/movement shooter/Scripts/MovementControllerMS.cs
@@ -5,6 +5,9 @@ public class MovementControllerMS : MonoBehaviour
     
   public Rigidbody rb;
   public float baseSpeed=100f;
+  public Transform camera;
+  public Transform player;
+  public Collider physicsColider;
   public float maxSpeed = 30f;
   public float maxslope=30f;
   public float maxWallSlopeVarience=15f;
@@ -20,12 +23,16 @@ public class MovementControllerMS : MonoBehaviour
   public float cooldown;
   public float dashCoolDown;
   Vector3 wallNormal;
-
+  float defaultMaxSpeed;
   // Start is called once before the first execution of Update after the MonoBehaviour is created
   void Start()
   {
     rb=GetComponent<Rigidbody>();
     jumpsLeft=jumpsMax;
+    defaultMaxSpeed=maxSpeed;
+    if (!physicsColider){
+      physicsColider = GetComponent<Collider>();
+    }
   }
 
   void OnCollisionStay(Collision other){
@@ -47,17 +54,16 @@ public class MovementControllerMS : MonoBehaviour
     }
   }
 
-  
-
   void OnCollisionExit(Collision Other){
     isGrounded=false;
     isWalled=false;
   }
 
   // Update is called once per frame
-  void Update()
-  {
+  void Update(){
     float speed;
+    float speeds=(new Vector3(rb.linearVelocity.x,0f,rb.linearVelocity.z)).magnitude;
+    
     if (dashesLeft<dashes){
       cooldown -= Time.deltaTime;
     } else {
@@ -67,7 +73,22 @@ public class MovementControllerMS : MonoBehaviour
       cooldown=dashCoolDown;
       dashesLeft++;
     }
-    
+    bool crouching = false;
+    if (Input.GetKey(KeyCode.LeftControl)){
+      float ply=0.5f;
+      float newPlayer=Mathf.MoveTowards(player.localScale.y,ply,ply*Time.deltaTime*2);
+      player.localScale = new Vector3(player.localScale.x, newPlayer, player.localScale.z);
+      maxSpeed=1000000000f;
+      physicsColider.material.dynamicFriction=0.1f;
+      crouching=true;
+    } else {
+      float ply=1f;
+      float newPlayer=Mathf.MoveTowards(player.localScale.y,ply,ply*Time.deltaTime*2);
+      player.localScale = new Vector3(player.localScale.x, newPlayer, player.localScale.z);
+      maxSpeed=defaultMaxSpeed;
+      physicsColider.material.dynamicFriction=0.8f;
+    }
+
     if (jumpsLeft>0&&Input.GetKeyDown(KeyCode.Space)){
       if (isWalled&& !isGrounded){
         rb.AddForce(wallNormal*60);
@@ -79,58 +100,60 @@ public class MovementControllerMS : MonoBehaviour
       jumpsLeft--;
     }
 
-    if (Input.GetKeyDown(KeyCode.LeftAlt)&&Input.GetKey(KeyCode.W)&&dashesLeft>0){
-      rb.AddRelativeForce(Vector3.forward*dashStrength, ForceMode.Impulse);
-      dashesLeft--;
-    }
-    if (Input.GetKeyDown(KeyCode.LeftAlt)&&Input.GetKey(KeyCode.A)&&dashesLeft>0){
-      rb.AddRelativeForce(Vector3.left*dashStrength, ForceMode.Impulse);
-      dashesLeft--;
-    }
-    if (Input.GetKeyDown(KeyCode.LeftAlt)&&Input.GetKey(KeyCode.S)&&dashesLeft>0){
-      rb.AddRelativeForce(Vector3.back*dashStrength, ForceMode.Impulse);
-      dashesLeft--;
-    }
-    if (Input.GetKeyDown(KeyCode.LeftAlt)&&Input.GetKey(KeyCode.D)&&dashesLeft>0){
-      rb.AddRelativeForce(Vector3.right*dashStrength, ForceMode.Impulse);
-      dashesLeft--;
-    }
+    if (!crouching){
 
-    if (Input.GetKey(KeyCode.LeftShift)){
-      speed=baseSpeed*2;
-    } else {
-      speed =baseSpeed;
-    }
+      if (Input.GetKeyDown(KeyCode.LeftAlt)&&Input.GetKey(KeyCode.W)&&dashesLeft>0){
+        rb.AddRelativeForce(isGrounded ? (Vector3.forward*dashStrength) : (Vector3.forward*dashStrength/2.5f), ForceMode.Impulse);
+        dashesLeft--;
+      }
+      if (Input.GetKeyDown(KeyCode.LeftAlt)&&Input.GetKey(KeyCode.A)&&dashesLeft>0){
+        rb.AddRelativeForce(isGrounded ? (Vector3.left*dashStrength) : (Vector3.forward*dashStrength/2.5f), ForceMode.Impulse);
+        dashesLeft--;
+      }
+      if (Input.GetKeyDown(KeyCode.LeftAlt)&&Input.GetKey(KeyCode.S)&&dashesLeft>0){
+        rb.AddRelativeForce(isGrounded ? (Vector3.back*dashStrength) : (Vector3.forward*dashStrength/2.5f), ForceMode.Impulse);
+        dashesLeft--;
+      }
+      if (Input.GetKeyDown(KeyCode.LeftAlt)&&Input.GetKey(KeyCode.D)&&dashesLeft>0){
+        rb.AddRelativeForce(isGrounded ? (Vector3.right*dashStrength) : (Vector3.forward*dashStrength/2.5f), ForceMode.Impulse);
+        dashesLeft--;
+      }
 
-    if (Input.GetKey(KeyCode.W)&&(isGrounded||isWalled)){
-      rb.AddRelativeForce(Vector3.forward * speed*Time.deltaTime, ForceMode.VelocityChange);
-    } else if (Input.GetKey(KeyCode.W)&&!(isGrounded||isWalled)&&(speed<maxSpeed*3f)){
-      rb.AddRelativeForce(Vector3.forward * speed/4f*Time.deltaTime, ForceMode.VelocityChange);
-    }
-    if (Input.GetKey(KeyCode.A)&&(isGrounded||isWalled)){
-      rb.AddRelativeForce(Vector3.left*speed*Time.deltaTime, ForceMode.VelocityChange);
-    } else if (Input.GetKey(KeyCode.A)&&!(isGrounded||isWalled)&&(speed<maxSpeed*3f)){
-      rb.AddRelativeForce(Vector3.left * speed/4f*Time.deltaTime, ForceMode.VelocityChange);
-    }
-    if (Input.GetKey(KeyCode.S)&&(isGrounded||isWalled)){
-      rb.AddRelativeForce(Vector3.back*speed*Time.deltaTime, ForceMode.VelocityChange);
-    } else if (Input.GetKey(KeyCode.S)&&!(isGrounded||isWalled)&&(speed<maxSpeed*3f)){
-      rb.AddRelativeForce(Vector3.back * speed/4f*Time.deltaTime, ForceMode.VelocityChange);
-    }
-    if (Input.GetKey(KeyCode.D)&&(isGrounded||isWalled)){
-      rb.AddRelativeForce(Vector3.right*speed*Time.deltaTime, ForceMode.VelocityChange);
-    } else if (Input.GetKey(KeyCode.D)&&!(isGrounded||isWalled)&&(speed<maxSpeed*3f)){
-      rb.AddRelativeForce(Vector3.right * speed/4f*Time.deltaTime, ForceMode.VelocityChange);
+      if (Input.GetKey(KeyCode.LeftShift)){
+        speed=baseSpeed*2;
+      } else {
+        speed =baseSpeed;
+      }
+
+      if (Input.GetKey(KeyCode.W)&&(isGrounded||isWalled)){
+        rb.AddRelativeForce(Vector3.forward * speed*Time.deltaTime, ForceMode.VelocityChange);
+      } else if (Input.GetKey(KeyCode.W)&&!(isGrounded||isWalled)&&(speeds<maxSpeed*3f)){
+        rb.AddRelativeForce(Vector3.forward * speed/2f*Time.deltaTime, ForceMode.VelocityChange);
+      }
+      if (Input.GetKey(KeyCode.A)&&(isGrounded||isWalled)){
+        rb.AddRelativeForce(Vector3.left*speed*Time.deltaTime, ForceMode.VelocityChange);
+      } else if (Input.GetKey(KeyCode.A)&&!(isGrounded||isWalled)&&(speeds<maxSpeed*3f)){
+        rb.AddRelativeForce(Vector3.left * speed/2f*Time.deltaTime, ForceMode.VelocityChange);
+      }
+      if (Input.GetKey(KeyCode.S)&&(isGrounded||isWalled)){
+        rb.AddRelativeForce(Vector3.back*speed*Time.deltaTime, ForceMode.VelocityChange);
+      } else if (Input.GetKey(KeyCode.S)&&!(isGrounded||isWalled)&&(speeds<maxSpeed*3f)){
+        rb.AddRelativeForce(Vector3.back * speed/2f*Time.deltaTime, ForceMode.VelocityChange);
+      }
+      if (Input.GetKey(KeyCode.D)&&(isGrounded||isWalled)){
+        rb.AddRelativeForce(Vector3.right*speed*Time.deltaTime, ForceMode.VelocityChange);
+      } else if (Input.GetKey(KeyCode.D)&&!(isGrounded||isWalled)&&(speeds<maxSpeed*3f)){
+        rb.AddRelativeForce(Vector3.right * speed/2f*Time.deltaTime, ForceMode.VelocityChange);
+      }
     }
 
 
     if (isGrounded){
-      float speeds = (new Vector3(rb.linearVelocity.x,0f,rb.linearVelocity.z)).magnitude;
       if (speeds >maxSpeed) {
         rb.AddForce(-(new Vector3(rb.linearVelocity.x,0f,rb.linearVelocity.z)).normalized * ((speeds - maxSpeed)/maxSpeed)*breakingStrength, ForceMode.Acceleration);
       }
     } else if (isWalled){
-      float speeds = (new Vector3(rb.linearVelocity.x,0f,rb.linearVelocity.z)).magnitude;
+      speeds = (new Vector3(rb.linearVelocity.x,0f,rb.linearVelocity.z)).magnitude;
       if (speeds >maxSpeed) {
         rb.AddForce(-(new Vector3(rb.linearVelocity.x,0f,rb.linearVelocity.z)).normalized * ((speeds - maxSpeed)/maxSpeed)*breakingStrength/2f, ForceMode.Acceleration);
       }
